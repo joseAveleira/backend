ac75b94c378121880298e084940dcd57b3897522
Jorge Rossi <jorgefzrossi@gmail.com>
2020-01-28 20:08:20 -0300

    Configurando persistência do mosca utilizando mongodb

--- src/broker.js
+++ src/broker.js
@@ -1,25 +1,24 @@
 const mosca = require('mosca')
 
-try {
-    const server = new mosca.Server({
-        port: 1883,
-        backend: {
-            type: 'mongo',
-            url: `mongodb://${process.env.NODE_ENV === 'production' ? 'mongodb' : 'localhost'}:27017/mqtt`,
-            pubsubCollection: 'scoreboard',
-            mongo: {}
-        },
-        http: {
-            port: 81,
-            bundle: true,
-            static: './'
-        },
-    })
+const server = new mosca.Server({
+    port: 1883,
+    backend: {
+        type: 'mongo',
+        url: `mongodb://${process.env.NODE_ENV === 'production' ? 'mongodb' : 'localhost'}:27017/mqtt`,
+        pubsubCollection: 'scoreboard',
+        mongo: {}
+    },
+    http: {
+        port: 81,
+        bundle: true,
+        static: './'
+    },
+    persistence: {
+        factory: mosca.persistence.Mongo,
+        url: `mongodb://${process.env.NODE_ENV === 'production' ? 'mongodb' : 'localhost'}:27017/mqtt`,
+    }
+})
 
-    server.on('ready', () => {
-        console.log("Broker executando")
-    })
-
-} catch (error) {
-    console.log('pei', error)
-}    
+server.on('ready', () => {
+    console.log("Broker executando")
+})
