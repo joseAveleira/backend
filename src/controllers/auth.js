142244a19782a88552147b85f5b68f570d1ddf2c
Jorge Rossi <jorgefzrossi@gmail.com>
2020-01-30 20:29:44 -0300

    Implementando rota para gerar novos tokens

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 
 const knex = require('../database')
 
@@ -27,6 +28,48 @@ class AuthController {
             const token = await jwt.sign(username, process.env.JWT_SECRET || 'secret')
 
             return res.status(200).json({ message: 'authentiacated', token })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
+
+    async refreshTokens(req, res) {
+        try {
+            const { scoreboard_topic: scoreboardTopic } = req.params
+            const { refresh_token: refreshToken } = req.query
+
+            if (!scoreboardTopic) {
+                return res.status(400).json({ message: 'invalid_scoreboard_topic' })
+            }
+
+            if (!refreshToken) {
+                return res.status(400).json({ message: 'invalid_refresh_token' })
+            }
+
+            const scoreboard = await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .first()
+
+            if (!scoreboard) {
+                return res.status(400).json({ message: 'scoreboard_not_found' })
+            }
+
+            if (scoreboard.refresh_token !== refreshToken) {
+                return res.status(401).json({ message: 'wrong_refresh_token' })
+            }
+
+            const newPublishToken = crypto.randomBytes(16).toString('hex');
+            const newRefreshToken = crypto.randomBytes(16).toString('hex');
+
+            await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .update({
+                    publish_token: newPublishToken,
+                    refresh_token: newRefreshToken
+                })
+
+            res.json({ message: 'tokens_refreshed', publish_token: newPublishToken, refresh_token: newRefreshToken })
+
         } catch (error) {
             res.status(500).json({ message: error })
         }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/auth')
 const router = express.Router()
 
 router.post('/signin', AuthController.signIn)
+router.get('/refresh-tokens/:scoreboard_topic', AuthController.refreshTokens)
 
 module.exports = router
