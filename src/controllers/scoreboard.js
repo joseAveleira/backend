04d6230bf213a6e7e99ad66978d949e31c8a46a3
Jorge Rossi <jorgefzrossi@gmail.com>
2020-01-30 20:34:30 -0300

    Implementando rota para o administrador tomar controle do placar

--- src/controllers/scoreboard.js
+++ src/controllers/scoreboard.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const knex = require('../database')
 
 class ScoreboardController {
@@ -18,6 +19,81 @@ class ScoreboardController {
             }))
 
             return res.status(200).json({ message: 'scoreboards found', data: scoreboards })
+        } catch (error) {
+            res.status(500).json({ message: error.toString() })
+        }
+    }
+
+    async refreshTokens(req, res) {
+        try {
+            const { scoreboard_topic: scoreboardTopic } = req.params
+            const { refresh_token: refreshToken } = req.query
+
+            if (!scoreboardTopic) {
+                return res.status(400).json({ message: 'invalid_scoreboard_topic' })
+            }
+
+            if (!refreshToken) {
+                return res.status(400).json({ message: 'invalid_refresh_token' })
+            }
+
+            const scoreboard = await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .first()
+
+            if (!scoreboard) {
+                return res.status(400).json({ message: 'scoreboard_not_found' })
+            }
+
+            if (scoreboard.refresh_token !== refreshToken) {
+                return res.status(401).json({ message: 'wrong_refresh_token' })
+            }
+
+            const newPublishToken = crypto.randomBytes(16).toString('hex');
+            const newRefreshToken = crypto.randomBytes(16).toString('hex');
+
+            await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .update({
+                    publish_token: newPublishToken,
+                    refresh_token: newRefreshToken
+                })
+
+            res.json({ message: 'tokens_refreshed', publish_token: newPublishToken, refresh_token: newRefreshToken })
+
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
+    }
+
+    async takeControl(req, res) {
+        try {
+            const { scoreboard_topic: scoreboardTopic } = req.params
+
+            if (!scoreboardTopic) {
+                return res.status(400).json({ message: 'invalid_scoreboard_topic' })
+            }
+
+            const scoreboard = await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .first()
+
+            if (!scoreboard) {
+                return res.status(400).json({ message: 'scoreboard_not_found' })
+            }
+
+            const newPublishToken = crypto.randomBytes(16).toString('hex');
+            const newRefreshToken = crypto.randomBytes(16).toString('hex');
+
+            await knex('scoreboards')
+                .where({ topic: scoreboardTopic })
+                .update({
+                    publish_token: newPublishToken,
+                    refresh_token: newRefreshToken
+                })
+
+            res.json({ message: 'tokens_refreshed', publish_token: newPublishToken, refresh_token: newRefreshToken })
+
         } catch (error) {
             res.status(500).json({ message: error.toString() })
         }
--- src/routes/scoreboard.js
+++ src/routes/scoreboard.js
@@ -5,5 +5,7 @@ const AdminAuthMiddleware = require('../middlewares/auth')
 const router = express.Router()
 
 router.get('/', AdminAuthMiddleware, ScoreboardController.index)
+router.get('/refresh-tokens/:scoreboard_topic', ScoreboardController.refreshTokens)
+router.post('/take-control/:scoreboard_topic', AdminAuthMiddleware, ScoreboardController.takeControl)
 
 module.exports = router
